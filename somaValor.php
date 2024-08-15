dde56027038fa18b5d27682c44819aeb1b8a8dec
RochaS18 <igorrochasilva152020.gmail.com>
2024-08-14 22:08:58 -0300

    estrutura de decisão

---
+++ somaValor.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = 20.50;
+$num2 = 10.15;
+$resp = 0.0;
+
+$resp = $num1 + $num2;
+
+if ($resp >= $num1) {
+	
+	echo "Respota é maior que o num1";
+}else{
+
+	echo "Respota é menor que num1";
+}
+
+
+?>
