ffc5e53c32b06fe4c71aa7aa91e3577cd8805528
RochaS18 <igorrochasilva152020@gmail.com>
2024-09-30 20:27:01 -0300

    Add files via upload

---
+++ time.php
@@ -0,0 +1,36 @@
+<?php
+
+//date_default_timezone_set('UTC');
+
+
+//$tempo = time();
+
+
+//$dataAtual = date("d/m/y");
+
+//echo "<br/>Hoje é: " .$dataAtual."<br/>";
+
+//$hora = date("h:i:s");
+
+//echo "<br/> Agora são: " .$hora."<br/>";
+
+//$dataHora = date("d/m/y h:i:s");
+
+//echo "<br/> Data e hora: " .$dataHora."<br/>";
+
+
+
+$num1 = 0 ;
+
+$num2 = 0 ;
+
+$resp = $num1 + $num2;
+
+echo("A soma de num1 + num2 é  $resp" );
+
+
+
+
+
+
+?>
