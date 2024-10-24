bd387752201ce4f048cfa23df9edf608b36dd5d6
RochaS18 <igorrochasilva152020@gmail.com>
2024-10-23 22:13:24 -0300

    problema um

---
+++ problema01/problema_01form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>	Probrema 01</title>
+
+</head>
+<body>
+		<form action="#" method="post">
+
+			<label>Nome:</label>
+			<input type="text" name="nome" autocomplete="off" maxlength="30"><br><br>
+
+			<label>Idade:</label>
+			<input type="text" name="idade" autocomplete="off" maxlength="30"><br><br>
+			
+			<label>Peso:</label>
+			<input type="text" name="peso" autocomplete="off" maxlength="30"><br><br>
+			
+			<label>Altura:</label>
+			<input type="text" name="altura" autocomplete="off" maxlength="30"><br><br>
+
+			<input type="submit" name="enviar" value="Enviar">
+
+			<a href = "problema01_form2.php"> Insira </a>
+			
+
+		</form>
+	
+
+</body>
+</html>
