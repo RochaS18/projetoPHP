a7bcd0277fa833a06254844df75fc6daa566a0da
RochaS18 <igorrochasilva152020@gmail.com>
2024-10-30 22:20:27 -0300

    Add files via upload

---
+++ formulario_B.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>	Probrema 01</title>
+
+</head>
+<body>
+		<form action="sesao" method="post">
+
+			<label>Nome:</label>
+			<input type="text" name="nome02" autocomplete="off" maxlength="30"><br><br>
+
+			<label>Idade:</label>
+			<input type="text" name="idade02" autocomplete="off" maxlength="30"><br><br>
+			
+			<label>Peso:</label>
+			<input type="text" name="peso02" autocomplete="off" maxlength="30"><br><br>
+			
+			<label>Altura:</label>
+			<input type="text" name="altura02" autocomplete="off" maxlength="30"><br><br>
+
+			<input type="submit" name="enviar" value="Enviar">
+	
+		
+			
+
+		</form>
+	
+
+</body>
+</html>
---
+++ sesao.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sessão do form1</title>
+<?php 	
+      
+session_start();
+
+
+$_SESSION["n"]= $_POST["nome"];
+
+
+$_SESSION["i"] = $_POST["idade"];
+
+
+$_SESSION['p'] = $_POST['peso'];
+
+
+$_SESSION['al'] = $_POST['altura'];
+
+
+$_SESSION["n2"]= $_POST["nome02"];
+
+
+$_SESSION["i2"] = $_POST["idade02"];
+
+
+$_SESSION['p2'] = $_POST['peso02'];
+
+
+$_SESSION['al2'] = $_POST['altura02'];
+
+	// idade
+
+	if($i > $i2){
+
+		$m_velho = $_SESSION['i'];		
+
+	} else{
+
+		$m_velho = $_SESSION['i2'];
+	}
+
+// altura 
+
+	if($al > $al2){
+
+		$al_maior = $_SESSION['al'];		
+
+	} else{
+
+		$al_maior = $_SESSION['al2'];
+	}
+
+
+// PESO
+
+	if($p > $p2){
+
+		$p_maior = $_SESSION['p'];		
+
+	} else{
+
+		$p_maior = $_SESSION['p2'];
+	}
+
+
+	
+	echo "Quem tem maior Idade". $m_velho;
+	 
+
+
+
+
+
+
+	 ?>
+
+
+
+</head>
+<body>
+
+
+
+
+</body>
+</html>
